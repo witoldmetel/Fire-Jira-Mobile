22f0f0127b44ce82d79859ff4564a985b1d61a1c
witoldmetel <witoldmetel@gmail.com>
2022-11-17 17:29:45 +0100

    Init React Native CLI

--- README.md
+++
@@ -1,5 +0,0 @@
-# Fire Zoom [BE]
-
-This is backend part of Fire Zoom.
-
-Front-End repo: https://github.com/witoldmetel/Fire-Zoom-FE
--- index.js
+++ index.js
@@ -1,46 +1,9 @@
-const express = require('express');
-const app = express();
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
+/**
+ * @format
+ */
 
-const port = 3001;
-let users = [];
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
 
-app.get('/', (req, res) => res.send('Fire Zoom Back-End'));
-
-const addUser = (userName, roomId) => {
-	users.push({ userName, roomId });
-};
-
-const removeUser = (userName) => {
-	users = users.filter((user) => user.userName !== userName);
-};
-
-const getRoomUsers = (roomId) => {
-	return users.filter((user) => user.roomId === roomId);
-};
-
-io.on('connection', (socket) => {
-	socket.on('join-room', ({ roomId, userName }) => {
-		if (roomId && userName) {
-			socket.join(roomId);
-
-			addUser(userName, roomId);
-
-			socket.to(roomId).emit('user-connected', userName);
-
-			io.to(roomId).emit('all-users', getRoomUsers(roomId));
-		}
-
-		socket.on('disconnect', () => {
-			socket.leave(roomId);
-
-			// User leave room
-			removeUser(userName);
-
-			io.to(roomId).emit('all-users', getRoomUsers(roomId));
-		});
-	});
-});
-
-server.listen(port, () => console.log(`Fire Zoom on port: ${port}`));
+AppRegistry.registerComponent(appName, () => App);
