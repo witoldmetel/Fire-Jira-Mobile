a445f8929eafb0e0fb9431371ef2718b57c0998f
witoldmetel <witoldmetel@gmail.com>
2023-02-13 17:15:39 +0100

    update slide title

--- src/authentication/onboarding/Slide/Slide.tsx
+++ src/authentication/onboarding/Slide/Slide.tsx
@@ -35,6 +35,9 @@ export const Slide = ({title, titlePosition, picture}: SlideProps) => {
               fontSize: getCustomFontSize(75),
               color: theme.palette.common.white,
               textAlign: 'center',
+              textShadowColor: '#585858',
+              textShadowOffset: {width: 5, height: 5},
+              textShadowRadius: 10,
             } as StyleProp<TextStyle>
           }>
           {title}
